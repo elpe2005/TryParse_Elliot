e481a85428ca581e186c9c75ef41faadd88578b8
elpe2005 <Epedersen2005@gmail.com>
2024-01-25 11:26:29 +0100

    inte klar

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int resultat;
+TryParse(Console.ReadLine(), out resultat);
+
+
+static bool TryParse(string s, out resultat)
+{
+    try
+    {
+        int i = int.Parse(s);
+    }
+    catch (FormatException)
+    {   
+        resultat = 0;
+        return false;
+    }
+    
+}
